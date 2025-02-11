7e28e52eb8390dbb9f47826704e9bd295e0a1ade
root <root@LAPTOP-F76KEGM7>
2025-02-11 21:20:13 +0800

    robot_base package source code modification finished

--- robot_base/src/robot_imu.cpp
+++ robot_base/src/robot_imu.cpp
@@ -1,9 +1,7 @@
-// 生成ros发布imu主题的代码
-#include <ros/ros.h>
-#include <sensor_msgs/Imu.h>
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/imu.hpp"
 
-
-// 生成c++串口初始化和读取代码
+// Standard C++ and POSIX headers for serial communication
 #include <iostream>
 #include <string>
 #include <unistd.h>
@@ -11,154 +9,162 @@
 #include <termios.h>
 #include <stdio.h>
 
-using namespace std;
+// For convenience
+using std::string;
+using std::cout;
+using std::endl;
+
+// Global serial file descriptor (could also be encapsulated in the class)
 int serial_fd = -1;
-int open_serial(string port, int baud_rate)
+
+/**
+ * @brief Opens the serial port and configures it.
+ * 
+ * @param port The serial port device (e.g., "/dev/IMU")
+ * @param baud_rate The desired baud rate (currently unused in this example since we hardcode B230400)
+ * @return int 0 on success, -1 on failure
+ */
+int open_serial(const string &port, int baud_rate)
 {
-    serial_fd = open(port.c_str(), O_RDWR | O_NOCTTY); // 打开串口
-    if (serial_fd == -1)
-    {
-        cout << "串口打开失败！" << endl;
-        return -1;
-    }
-    else
-    {
-        cout << "串口打开成功！" << endl;
-    }
-    struct termios options; // 串口配置结构体
-    tcgetattr(serial_fd, &options);
-    options.c_cflag |= CLOCAL; // 忽略调制解调器状态行
-    options.c_cflag |= CREAD;  // 启用接收器
-    options.c_cflag &= ~CSIZE; // 字符长度掩码。取值为CS5, CS6, CS7或CS8
-    options.c_cflag |= CS8;    // 8位数据位
-    options.c_cflag &= ~PARENB; // 校验位
-    options.c_cflag &= ~CSTOPB; // 停止位
-    options.c_iflag |= IGNPAR;  // 忽略帧错误和奇偶校验错
-    options.c_oflag = 0;        // 输出模式
-    options.c_lflag = 0;        // 不激活终端模式
-    options.c_cc[VTIME] = 0; // 读取一个字符等待1*(1/10)s
-    options.c_cc[VMIN] = 1;  // 读取字符的最少个数为1
-    cfsetospeed(&options, B230400); // 设置波特率为115200
-    cfsetispeed(&options, B230400);
-    tcflush(serial_fd, TCIFLUSH); // 清空输入缓存区
-    if (tcsetattr(serial_fd, TCSANOW, &options) != 0) // TCSANOW：不等数据传输完毕就立即改变属性
-    {
-        cout << "imu串口设置失败！" << endl;
-        return -1;
-    }
-    else
-    {
-        cout << "imu串口设置成功！" << endl;
-    }
-    return 0;
+  serial_fd = open(port.c_str(), O_RDWR | O_NOCTTY);
+  if (serial_fd == -1)
+  {
+    cout << "串口打开失败！" << endl;
+    return -1;
+  }
+  else
+  {
+    cout << "串口打开成功！" << endl;
+  }
+  struct termios options;
+  tcgetattr(serial_fd, &options);
+  options.c_cflag |= CLOCAL;
+  options.c_cflag |= CREAD;
+  options.c_cflag &= ~CSIZE;
+  options.c_cflag |= CS8;
+  options.c_cflag &= ~PARENB;
+  options.c_cflag &= ~CSTOPB;
+  options.c_iflag |= IGNPAR;
+  options.c_oflag = 0;
+  options.c_lflag = 0;
+  options.c_cc[VTIME] = 0;
+  options.c_cc[VMIN] = 1;
+  cfsetospeed(&options, B230400);
+  cfsetispeed(&options, B230400);
+  tcflush(serial_fd, TCIFLUSH);
+  if (tcsetattr(serial_fd, TCSANOW, &options) != 0)
+  {
+    cout << "imu串口设置失败！" << endl;
+    return -1;
+  }
+  else
+  {
+    cout << "imu串口设置成功！" << endl;
+  }
+  return 0;
 }
 
-// int read_serial(char *buffer, int buffer length)
-// {
-//     int bytes_read = 0;
-//     int bytes_count = 0;
-//     while (bytes_count < buffer_length)
-//     {
-//         bytes_read = read(serial_fd, buffer + bytes_count, buffer_length - bytes_count);
-//         if (bytes_read > 0)
-//         {
-//             bytes_count += bytes_read;
-//         }
-//         else
-//         {
-//             cout << "串口读取失败！" << endl;
-//             return -1;
-//         }
-//     }
-//     return 0;
-// }
+class RobotImuNode : public rclcpp::Node
+{
+public:
+  RobotImuNode()
+  : Node("imu_pub")
+  {
+    // Create a publisher for sensor_msgs::msg::Imu on the "imu" topic.
+    imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>("imu", 10);
 
+    // Open the serial port.
+    if (open_serial("/dev/IMU", 230400) != 0) {
+      RCLCPP_ERROR(this->get_logger(), "Failed to open the IMU serial port!");
+      rclcpp::shutdown();
+      return;
+    }
+
+    // Set up the fixed header fields for the IMU message.
+    imu_msg_.header.frame_id = "imu_link";
+    imu_msg_.orientation.x = 0.0;
+    imu_msg_.orientation.y = 0.0;
+    imu_msg_.orientation.z = 0.0;
+    imu_msg_.orientation.w = 1.0;
+  }
+
+  /**
+   * @brief The main loop that reads data from the serial port and publishes IMU messages.
+   */
+  void run()
+  {
+    // Buffer for reading serial data.
+    unsigned char buffer[16];
+    // Temporary arrays for sensor data.
+    short ax[6] = {0};
+    short gx[6] = {0};
+    // Loop until ROS 2 is shutdown.
+    while (rclcpp::ok())
+    {
+      // Read one byte from the serial port.
+      if (read(serial_fd, buffer, 1) <= 0) {
+        continue;
+      }
+      if (buffer[0] == 0x55)
+      {
+        // Read the second byte.
+        if (read(serial_fd, buffer + 1, 1) <= 0) {
+          continue;
+        }
+        // If the second byte indicates accelerometer data.
+        if (buffer[1] == 0x51)
+        {
+          // Read the next 9 bytes into ax.
+          if (read(serial_fd, ax, 9) <= 0) {
+            continue;
+          }
+          // Process accelerometer data.
+          imu_msg_.linear_acceleration.x = ax[0] / 32768.0 * 16.0 * 9.80665;
+          imu_msg_.linear_acceleration.y = ax[1] / 32768.0 * 16.0 * 9.80665;
+          imu_msg_.linear_acceleration.z = ax[2] / 32768.0 * 16.0 * 9.80665;
+          // Adjust sign if needed.
+          imu_msg_.linear_acceleration.y = -imu_msg_.linear_acceleration.y;
+          // Set the covariance for the linear acceleration.
+          imu_msg_.linear_acceleration_covariance = {1e1, 0, 0,
+                                                      0, 1e-1, 0,
+                                                      0, 0, 1e-1};
+          // (Optionally, you might choose to publish here or wait for gyro data.)
+        }
+        // If the second byte indicates gyroscope data.
+        else if (buffer[1] == 0x52)
+        {
+          if (read(serial_fd, gx, 9) <= 0) {
+            continue;
+          }
+          imu_msg_.angular_velocity.x = gx[0] / 32768.0 * 2000 * (3.1415926535 / 180.0);
+          imu_msg_.angular_velocity.y = gx[1] / 32768.0 * 2000 * (3.1415926535 / 180.0);
+          imu_msg_.angular_velocity.z = gx[2] / 32768.0 * 2000 * (3.1415926535 / 180.0);
+          // Set the covariance for the angular velocity.
+          imu_msg_.angular_velocity_covariance = {1e6, 0, 0,
+                                                    0, 1e6, 0,
+                                                    0, 0, 1e-6};
+          // Update the timestamp in the header.
+          imu_msg_.header.stamp = this->now();
+          // Publish the complete IMU message.
+          imu_pub_->publish(imu_msg_);
+        }
+        // (Additional data types, such as magnetic field or orientation, could be processed here.)
+      }
+      // Optionally sleep or yield if needed.
+    }
+    close(serial_fd);
+  }
+
+private:
+  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
+  sensor_msgs::msg::Imu imu_msg_;
+};
 
 int main(int argc, char **argv)
 {
-    unsigned char buffer[16];
-    short ax[6] = {0};
-    short gx[6] = {0};
-    short sAngle[6] = {0};
-    sensor_msgs::Imu imu_msg; // 声明一个名为imu_msg的imu消息
-    // sensor_msgs::MagneticField mag_msg; // 声明一个名为imu_msg的imu消息
-
-    ros::init(argc, argv, "imu_pub"); // 初始化ROS节点
-    ros::init(argc, argv, "mag_pub"); // 初始化ROS节点
-    ros::NodeHandle nh;               // 创建节点句柄
-    ros::Publisher imu_pub = nh.advertise<sensor_msgs::Imu>("imu", 10); // 创建一个Publisher，发布名为/imu的topic，消息类型为sensor_msgs::Imu，队列长度1000
-  
-    // ros::Rate loop_rate(10); // 设置循环的频率为10Hz
-    open_serial("/dev/IMU", 230400);
-
-    imu_msg.header.frame_id = "imu_link";   // 填充imu_msg的头部坐标系为base_link
-    imu_msg.orientation.x = 0.0;             // 填充imu_msg的四元数
-    imu_msg.orientation.y = 0.0;
-    imu_msg.orientation.z = 0.0;
-    imu_msg.orientation.w = 1.0;
-
-    while (ros::ok())
-    {
-        read(serial_fd, buffer, 1);
-        if (buffer[0] == 0x55)
-        {
-            // ROS_INFO("get date");
-            read(serial_fd, (buffer + 1) , 1);
-            if (buffer[1] == 0x51)
-            {
-                read(serial_fd, ax, 9);
-                // cout << "ax: " << ax[0] / 32768.0 * 16 << endl;
-                // cout << "ay: " << ax[1] / 32768.0 * 16 << endl;
-                // cout << "az: " << ax[2] / 32768.0 * 16 << endl;
-                imu_msg.linear_acceleration.x = ax[0] / 32768.0 * 16.0 * 9.80665; // 填充imu_msg的线性加速度
-                imu_msg.linear_acceleration.y = ax[1] / 32768.0 * 16.0 * 9.80665;
-                imu_msg.linear_acceleration.z = ax[2] / 32768.0 * 16.0 * 9.80665;
-
-                // //旋转180度
-                // imu_msg.linear_acceleration.x = - imu_msg.linear_acceleration.x;
-                imu_msg.linear_acceleration.y = - imu_msg.linear_acceleration.y;
-                imu_msg.linear_acceleration_covariance =  {1e1,0,0,
-                                                            0,1e-1,0,
-                                                            0,0,1e-1};
-                                                            
-                //imu_pub.publish(imu_msg); // 发布imu消息
-            }
-            else if (buffer[1] == 0x52)
-            {
-                read(serial_fd, gx, 9);
-                // cout << "gx: " << gx[0] / 32768.0 * 2000 << endl;
-                // cout << "gy: " << gx[1] / 32768.0 * 2000 << endl;
-                // cout << "gz: " << gx[2] / 32768.0 * 2000 << endl;
-                // imu_msg.angular_velocity.x = gx[0] / 32768.0 * 2000 ; // 填充imu_msg的角速度
-                // imu_msg.angular_velocity.y = gx[1] / 32768.0 * 2000 ;
-                // imu_msg.angular_velocity.z = gx[2] / 32768.0 * 2000 ;
-                imu_msg.angular_velocity.x = gx[0] / 32768.0 * 2000 * ( 3.1415926535 / 180.0); // 填充imu_msg的角速度
-                imu_msg.angular_velocity.y = gx[1] / 32768.0 * 2000 * ( 3.1415926535 / 180.0);
-                imu_msg.angular_velocity.z = gx[2] / 32768.0 * 2000 * ( 3.1415926535 / 180.0);
-                // //旋转180度
-                // imu_msg.angular_velocity.x = - imu_msg.angular_velocity.x;
-                // imu_msg.angular_velocity.y = - imu_msg.angular_velocity.y;
-                imu_msg.angular_velocity_covariance = {1e6, 0, 0,
-                                                        0, 1e6, 0, 
-                                                        0, 0, 1e-6};
-
-
-                imu_pub.publish(imu_msg); // 发布imu消息
-            }
-	    // else if (buffer[1] == 0x53)
-        //     {
-        //         read(serial_fd, sAngle, 9);
-		//         imu_msg.header.stamp = ros::Time::now(); // 填充imu_msg的头部时间戳
-        //         imu_msg.orientation.x = sAngle[0] / 32768.0 * 180.0; // 填充imu_msg的角度
-        //         imu_msg.orientation.y = sAngle[1] / 32768.0 * 180.0;
-        //         imu_msg.orientation.z = sAngle[2] / 32768.0 * 180.0;
-	    //         imu_pub.publish(imu_msg); // 发布imu消息
-        //     }
-	
-        }
-        // ros::spinOnce();          // 处理ROS的信息，比如订阅消息,并调用回调函数
-        // loop_rate.sleep();        // 按照循环频率延时
-    }
-    close(serial_fd);
-    return 0;
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<RobotImuNode>();
+  node->run();
+  rclcpp::shutdown();
+  return 0;
 }
--- robot_base/src/robot_rod_cmd.cpp
+++ robot_base/src/robot_rod_cmd.cpp
@@ -1,18 +1,28 @@
-#include "ros/ros.h"
-#include "std_msgs/Int8.h"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int8.hpp"
 
-int main(int argc, char  *argv[])
+int main(int argc, char *argv[])
 {
-    /* code */
-    setlocale(LC_ALL,"");
-    ros::init(argc,argv,"robot_rod_cmd");
-    ros::NodeHandle nh;
-    ros::Publisher pub;
-    pub = nh.advertise<std_msgs::Int8>("rod_cmd", 1);	
-    std_msgs::Int8 cmd;
-    cmd .data=0;
-    pub.publish(cmd);
-    ros::spinOnce();
-
-    return 0;
+  // Initialize ROS 2
+  rclcpp::init(argc, argv);
+  
+  // Create a node with the name "robot_rod_cmd"
+  auto node = rclcpp::Node::make_shared("robot_rod_cmd");
+  
+  // Create a publisher for std_msgs/msg/Int8 on the "rod_cmd" topic with a queue size of 1
+  auto publisher = node->create_publisher<std_msgs::msg::Int8>("rod_cmd", 1);
+  
+  // Create and initialize the message
+  std_msgs::msg::Int8 cmd;
+  cmd.data = 0;
+  
+  // Publish the message
+  publisher->publish(cmd);
+  
+  // Process any pending callbacks (if needed)
+  rclcpp::spin_some(node);
+  
+  // Shutdown ROS 2
+  rclcpp::shutdown();
+  return 0;
 }
