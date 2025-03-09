f190f09b1e7b4e5c62e2fc7286e0db5eb90917ea
potato <hshen@LAPTOP-F76KEGM7>
2025-03-09 16:19:13 +0800

    ekf can be used in both real and simulation without changing paremeters

--- robot_base/launch/ekf_imu_odom.launch.py
+++ robot_base/launch/ekf_imu_odom.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
@@ -15,6 +16,13 @@ def generate_launch_description():
         description='Use simulation time if true'
     )
     
+    use_simulation = LaunchConfiguration('use_simulation')
+    use_simulation_arg = DeclareLaunchArgument(
+        'use_simulation',
+        default_value='false',
+        description='Use simulation environment if true'
+    )
+    
     # 获取参数文件路径
     config_file_path = os.path.join(
         get_package_share_directory('robot_base'),
@@ -22,19 +30,43 @@ def generate_launch_description():
         'ekf_config.yaml'
     )
     
+    # 创建EKF节点（模拟环境）
+    ekf_node_sim = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_imu_odom',
+        output='screen',
+        parameters=[
+            config_file_path,
+            {'use_sim_time': use_sim_time}  # 添加use_sim_time参数
+        ],
+        remappings=[
+            ('odometry/filtered', 'odom'),  # EKF节点的输出重映射到/odom
+            ('/odom_input', '/diff_drive/odom')  # 在模拟环境中，将/odom_input重映射到/diff_drive/odom
+        ],
+        condition=IfCondition(use_simulation)  # 仅在use_simulation为true时启动
+    )
+    
+    # 创建EKF节点（实际机器人环境）
+    ekf_node_real = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_imu_odom',
+        output='screen',
+        parameters=[
+            config_file_path,
+            {'use_sim_time': use_sim_time}  # 添加use_sim_time参数
+        ],
+        remappings=[
+            ('odometry/filtered', 'odom'),  # EKF节点的输出重映射到/odom
+            ('/odom_input', '/odom_wheel')  # 在实际环境中，将/odom_input重映射到/odom_wheel
+        ],
+        condition=UnlessCondition(use_simulation)  # 仅在use_simulation为false时启动
+    )
+    
     return LaunchDescription([
         use_sim_time_arg,
-        Node(
-            package='robot_localization',
-            executable='ekf_node',
-            name='ekf_imu_odom',
-            output='screen',
-            parameters=[
-                config_file_path,
-                {'use_sim_time': use_sim_time}  # 添加use_sim_time参数
-            ],
-            remappings=[
-                ('odometry/filtered', 'odom')  # EKF节点的输出重映射到/odom
-            ]
-        )
+        use_simulation_arg,
+        ekf_node_sim,
+        ekf_node_real
     ]) 
